f60b6ab124fb3f19cd474e49434f2e22ec128dff
Mario Bielert <mario.bielert@googlemail.com>
2017-11-23 14:37:56 +0100

    Update Nitro and use jiffies

---
+++ include/lmgd/log.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <nitro/log/attribute/jiffy.hpp>
+#include <nitro/log/attribute/severity.hpp>
+#include <nitro/log/filter/severity_filter.hpp>
+#include <nitro/log/log.hpp>
+#include <nitro/log/sink/stdout.hpp>
+
+namespace lmgd
+{
+
+namespace detail
+{
+
+    using record = nitro::log::record<nitro::log::tag_attribute, nitro::log::message_attribute,
+                                      nitro::log::severity_attribute, nitro::log::jiffy_attribute>;
+
+    template <typename Record>
+    class log_formater
+    {
+    public:
+        std::string format(Record& r)
+        {
+            std::stringstream s;
+
+            s << "[" << r.jiffy() << "][";
+
+            if (!r.tag().empty())
+            {
+                s << r.tag() << "[";
+            }
+
+            s << r.severity() << "]: " << r.message();
+
+            return s.str();
+        }
+    };
+
+    template <typename Record>
+    using log_filter = nitro::log::filter::severity_filter<Record>;
+}
+
+using Log = nitro::log::logger<detail::record, detail::log_formater, nitro::log::sink::StdOut,
+                               detail::log_filter>;
+}
